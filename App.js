811e252ea6f67677142740f186750c379e3bb683
p4ckman <90421233+jonathan-packham@users.noreply.github.com>
2023-02-22 00:26:21 -0700

    work in progress, v1.0

--- App.js
+++ App.js
@@ -1,20 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import React, {Component} from 'react'; 
+import {Button, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class redLight extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <TouchableOpacity
+        style={[styles.light, {backgroundColor: 'red'}]}
+      />
+    )
+  }
+}
+
+class blueLight extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    <TouchableOpacity
+      style={[styles.light, {backgroundColor: 'blue'}]}
+    />
+  }
+}
+
+class greenLight extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    <TouchableOpacity
+      style={[styles.light, {backgroundColor: 'green'}]}
+    />
+  }
+}
+
+class yellowLight extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    <TouchableOpacity
+      style={[styles.light, {backgroundColor: 'yellow'}]}
+    />
+  }
+}
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      times
+    }
+  }
 }
 
 const styles = StyleSheet.create({
+  btnContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  light: {
+    width: 100,
+    height: 100,
+    borderRadius: 20,
+  },
+  simonLook: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+    borderWidth: 5,
+    backgroundColor: 'black',
   },
 });
